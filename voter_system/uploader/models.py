e82e977d48772e0b7aa79032ffcbf6aa30bf3eff
Elijah Apreko <elijahapreko@gmail.com>
2024-09-17 05:43:39 +0000

    update model

--- voter_system/uploader/models.py
+++ voter_system/uploader/models.py
@@ -6,7 +6,8 @@ class Student(models.Model):
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
     department = models.CharField(max_length=100)
+    is_eligible = models.BooleanField(default=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.student_id
+        return f'{self.first_name} {self.last_name} ({self.student_id})'
